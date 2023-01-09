0b4aeb590531327e2e6a8e1f786bf28942e96d0d
Mizna Uppal <miznauppal@gmail.com>
2023-01-09 00:48:06 +0000

    clear

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React, { useState, useRef } from 'react'
-// import Artist from "./components/Artist";
-// import AlbumForm from './components/AlbumForm';
-// import AlbumList from "./components/AlbumList";
-// import Navbar from "./components/NavBar";
-// import Welcome from "./components/Welcome";
-// import Speaker from "./components/Speaker";
-
 import {AlbumForm, AlbumItem, AlbumList, Artist, NavBar, Speaker, Welcome} from './components/index';
 
 import { Routes ,Route } from 'react-router-dom';
@@ -28,53 +21,20 @@ const App = (like, setLike) => {
 
     return (
     <>
-       {/* <div class="speaker2" > </div> */}
-
-<div id="wrap">
-    <div id="maindiv" ref={divRef}>
-    <NavBar id="navbartop" />
-
-    <button id="scrollbtn" type="button" onClick={() => scrollToTop()}>
-    ↑
-      </button>
-    <Speaker  />
-  
-            <Routes>
-                <Route exact path="/" element={<Welcome />} />
-                <Route exact path="/albums" element={<AlbumList albums={albums} updateAlbums={updateAlbums} />} />
-                <Route exact path="/new" element={<AlbumForm addAlbum={addAlbum}/>}/>
-            </Routes>
-    <div>
-        {/* <Artist name="Random Artists Spotify" genre="Mix" intro="This is a really cool website where you can like stuff." /> */}
-        {/* <AlbumList albums={albums} updateAlbums={updateAlbums} /> */}
-    </div>
-    </div>
-    </div>
+    <div id="wrap">
+        <div id="maindiv" ref={divRef}>
+        <NavBar id="navbartop" />
+        <button id="scrollbtn" type="button" onClick={() => scrollToTop()}>↑</button>
+        <Speaker  />
+                <Routes>
+                    <Route exact path="/" element={<Welcome />} />
+                    <Route exact path="/albums" element={<AlbumList albums={albums} updateAlbums={updateAlbums} />} />
+                    <Route exact path="/new" element={<AlbumForm addAlbum={addAlbum}/>}/>
+                </Routes>
+        </div>
+        </div>
     </>
   )
 };
 
 export default App;
-
-// import React from 'react';
-// import Song from './components/Song';
-// import Artist from "./components/Artist";
-// import './App.css';
-// // import './components/MusicPlayer';
-
-// const App = () => {
-//     return(
-//         <>
-//         {/* <div className="container" >
-// 			<Header/>
-// 			<Container/>
-// 		</div> */}
-//         <Artist />
-//         <Song />
-//         </>
-//     );
-// }
-
-// // ReactDOM.render(<Index />, document.getElementById("root"));
-// export default App;
-
